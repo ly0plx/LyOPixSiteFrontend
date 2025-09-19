e10f641772f8e661510efcc96d19a787f4cc931a
Tanner <tanner.ordonez@gmail.com>
2025-09-18 19:49:02 -0600

    Finish Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,44 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <h1>LyOPix</h1>
+      <ul>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+            <FontAwesomeIcon icon={faHome} />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={({ isActive }) => (isActive ? "active" : "")}>
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/blog" className={({ isActive }) => (isActive ? "active" : "")}>
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/portfolio" className={({ isActive }) => (isActive ? "active" : "")}>
+            Portfolio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/products" className={({ isActive }) => (isActive ? "active" : "")}>
+            Products
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
