d5c74c8362f54b8339f3a90cf59303f1179b0880
Tanner <tanner.ordonez@gmail.com>
2025-09-26 21:22:14 -0600

    get ready for tree

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,34 @@ import "../styles/About.css";
 import { Helmet } from "react-helmet-async";
 
 export default function About() {
+  // Add MD support when implementing backend (https://chatgpt.com/share/68d72166-94fc-8006-a695-44ccd5ae539f)
   return (
     <div className="about-page">
       <Helmet>
         <title>About | LyOPix</title>
       </Helmet>
-      <h2>About LyOPix</h2>
-      <p>Some info goes here...</p>
+      <div className="about">
+        <h1>About LyOPix</h1>
+        <p>[TODO: Replace this with dump from backend]Est quis commodo et qui cupidatat ea eu aliqua. Enim et sunt consequat Lorem amet tempor commodo ut nulla sit elit ullamco sunt. Officia esse fugiat proident occaecat. Sit adipisicing aliquip labore duis magna voluptate. Minim labore ipsum ad ipsum nostrud et labore ut non proident. Mollit ad occaecat velit ullamco aliqua culpa voluptate. Incididunt fugiat sint est reprehenderit irure id excepteur.
+
+Eiusmod adipisicing velit non sunt. Reprehenderit in deserunt nostrud consectetur consequat est aute dolore quis eiusmod sint. Est occaecat ea est pariatur commodo eiusmod nulla laborum occaecat ullamco aliqua cupidatat veniam aute. In do do ea do irure proident enim.
+
+Qui cillum id sint sint magna incididunt consectetur. Fugiat qui nisi fugiat id ipsum ea occaecat sunt qui nisi nulla magna esse. Esse mollit adipisicing officia adipisicing elit eu esse pariatur voluptate elit sint deserunt. Est esse minim amet laboris nulla aliqua fugiat culpa ea culpa enim. Mollit quis id anim proident excepteur culpa dolore ullamco. Amet ut dolor aliqua pariatur laboris id do laborum amet proident ullamco veniam sint elit. Ut enim id ad ex quis.
+
+Amet id id laboris exercitation velit fugiat. Tempor deserunt magna et do non et ipsum et est. In ullamco esse mollit laboris ex ipsum exercitation pariatur.
+
+Amet nulla esse commodo cupidatat eu id aliquip dolore et dolore. Ipsum dolore laboris proident laborum laboris ad amet aute elit proident sunt dolor ut ea. Adipisicing minim commodo dolor Lorem aute in. Occaecat minim ea sunt eu id.
+
+Veniam officia aliquip sint eu. Id culpa voluptate ipsum deserunt cupidatat veniam ut pariatur cillum eiusmod aliquip. Non aliqua id nisi aliqua cillum dolore elit excepteur ad exercitation eu. Pariatur sint dolore in eiusmod consectetur ullamco cillum consequat.
+
+Laborum tempor laboris cillum fugiat nisi sit tempor. Voluptate ea in tempor fugiat mollit amet irure exercitation aliqua id. Reprehenderit est mollit velit dolor nostrud duis veniam reprehenderit anim nostrud culpa consectetur officia. Laboris pariatur dolore exercitation velit amet nisi aliquip minim proident Lorem tempor. Mollit dolor qui ullamco reprehenderit est est. In aliqua velit cillum non consequat.
+
+Duis ullamco dolore minim quis labore enim. Elit officia sit et laborum eiusmod eu commodo pariatur velit aliquip adipisicing Lorem. Proident incididunt dolor velit ex pariatur.
+
+Id ullamco tempor eu ipsum dolor reprehenderit. Dolor aliquip velit aute magna consectetur. Occaecat cupidatat commodo ullamco ipsum ea sit minim dolor ullamco. Esse eiusmod excepteur sint pariatur aliqua amet eiusmod sunt pariatur laborum. Velit cupidatat mollit mollit consectetur exercitation labore id minim anim incididunt do adipisicing exercitation. Occaecat duis consectetur in quis cupidatat cupidatat culpa elit laboris incididunt. Proident aliquip nulla sint irure fugiat voluptate.
+
+Adipisicing cupidatat consectetur ex incididunt nisi. Pariatur cupidatat aute anim enim sunt commodo. Quis pariatur aute laborum quis sunt commodo est amet irure laborum.</p>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet-async";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import avatar from "../assets/avatar.png";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </Helmet>
       <div className="hero">
         <div className="main">
-          <img src="../assets/avatar.png" alt="" />
+          <img src={avatar} alt="Avatar" />
           <h1>Hey! I'm LyOPix!</h1>
           <p>
             I'm a software developer that makes everything custom so that you
@@ -27,8 +28,12 @@ export default function Home() {
       <hr />
       <div className="mee">
         <h2>Check Me Out:</h2>
-        <Link to="/about" className="btn">About</Link>
-        <Link to="/portfolio" className="btn">Portfolio</Link>
+        <Link to="/about" className="btn">
+          About
+        </Link>
+        <Link to="/portfolio" className="btn">
+          Portfolio
+        </Link>
       </div>
       <hr />
       <div className="projects">
@@ -36,33 +41,78 @@ export default function Home() {
         <div className="project-list">
           <div className="project-card">
             <h3>Example Project Name</h3>
-            <h5>Status: <span>Finished</span></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi, quidem aut est necessitatibus quasi sapiente? Fugit nemo rerum molestias quidem quas, perspiciatis est eius culpa laborum saepe, deleniti alias?</p>
-            <a href="https://example.com/" className="btn">Example Button Text</a>
+            <h5>
+              Status: <span>Finished</span>
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos animi, quidem aut est necessitatibus quasi sapiente?
+              Fugit nemo rerum molestias quidem quas, perspiciatis est eius
+              culpa laborum saepe, deleniti alias?
+            </p>
+            <a href="https://example.com/" className="btn">
+              Example Button Text
+            </a>
           </div>
           <div className="project-card">
             <h3>Example Project Name</h3>
-            <h5>Status: <span>Finished</span></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi, quidem aut est necessitatibus quasi sapiente? Fugit nemo rerum molestias quidem quas, perspiciatis est eius culpa laborum saepe, deleniti alias?</p>
-            <a href="https://example.com/" className="btn">Example Button Text</a>
+            <h5>
+              Status: <span>Finished</span>
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos animi, quidem aut est necessitatibus quasi sapiente?
+              Fugit nemo rerum molestias quidem quas, perspiciatis est eius
+              culpa laborum saepe, deleniti alias?
+            </p>
+            <a href="https://example.com/" className="btn">
+              Example Button Text
+            </a>
           </div>
           <div className="project-card">
             <h3>Example Project Name</h3>
-            <h5>Status: <span>Finished</span></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi, quidem aut est necessitatibus quasi sapiente? Fugit nemo rerum molestias quidem quas, perspiciatis est eius culpa laborum saepe, deleniti alias?</p>
-            <a href="https://example.com/" className="btn">Example Button Text</a>
+            <h5>
+              Status: <span>Finished</span>
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos animi, quidem aut est necessitatibus quasi sapiente?
+              Fugit nemo rerum molestias quidem quas, perspiciatis est eius
+              culpa laborum saepe, deleniti alias?
+            </p>
+            <a href="https://example.com/" className="btn">
+              Example Button Text
+            </a>
           </div>
           <div className="project-card">
             <h3>Example Project Name</h3>
-            <h5>Status: <span>Finished</span></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi, quidem aut est necessitatibus quasi sapiente? Fugit nemo rerum molestias quidem quas, perspiciatis est eius culpa laborum saepe, deleniti alias?</p>
-            <a href="https://example.com/" className="btn">Example Button Text</a>
+            <h5>
+              Status: <span>Finished</span>
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos animi, quidem aut est necessitatibus quasi sapiente?
+              Fugit nemo rerum molestias quidem quas, perspiciatis est eius
+              culpa laborum saepe, deleniti alias?
+            </p>
+            <a href="https://example.com/" className="btn">
+              Example Button Text
+            </a>
           </div>
           <div className="project-card">
             <h3>Example Project Name</h3>
-            <h5>Status: <span>Finished</span></h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos animi, quidem aut est necessitatibus quasi sapiente? Fugit nemo rerum molestias quidem quas, perspiciatis est eius culpa laborum saepe, deleniti alias?</p>
-            <a href="https://example.com/" className="btn">Example Button Text</a>
+            <h5>
+              Status: <span>Finished</span>
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos animi, quidem aut est necessitatibus quasi sapiente?
+              Fugit nemo rerum molestias quidem quas, perspiciatis est eius
+              culpa laborum saepe, deleniti alias?
+            </p>
+            <a href="https://example.com/" className="btn">
+              Example Button Text
+            </a>
           </div>
         </div>
       </div>
